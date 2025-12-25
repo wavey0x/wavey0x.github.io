7fca5835b746a09f10f9f868c28eba89c21f2557
wavey0x <retro.waveyy@gmail.com>
2025-12-25 15:29:51 -0500

    mobile styling

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mode_toggle = document.getElementById("light-toggle");
+    const mode_toggle_mobile = document.getElementById("light-toggle-mobile");
 
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
-    });
+    if (mode_toggle) {
+        mode_toggle.addEventListener("click", function() {
+            toggleTheme(localStorage.getItem("theme"));
+        });
+    }
+
+    if (mode_toggle_mobile) {
+        mode_toggle_mobile.addEventListener("click", function() {
+            toggleTheme(localStorage.getItem("theme"));
+        });
+    }
 });
 
